d638e6cba5c34766b2de81dc08d4a084f1bbf989
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-10-01 16:13:58 +0200

    componente App con stato e logica di filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, userEffect } from "react";
+import { useEffect, useState, userEffect } from "react";
 import MovieList from "./components/MovieList";
 import GenreFilter from "./components/GenreFilter";
 import "./App.css";
@@ -12,8 +12,38 @@ const initialMovies = [
   { title: "Pulp Fiction", genre: "Thriller" },
 ];
 
+const genres = ["tutti", ...new Set(initialMovies.map((movie) => movie.genre))];
+
 function App() {
-  return <></>;
+  // stati per memorizzzare il genere attualmente selezionato e per memorizzare la lista dei film da visualizzare//
+  const [selectedGenre, setSelectedGenre] = useState("tutti");
+  const [filteredMovies, setFilteredMovies] = useState(initialMovies);
+
+  //esegue la logica di filtro ogni volta che `selectedGenre` cambia
+  useEffect(() => {
+    if (selectedGenre === "tutti") {
+      setFilteredMovies(initialMovies);
+    } else {
+      const filtered = initialMovies.filter(
+        (movie) => movie.genre === selectedGenre
+      );
+      setFilteredMovies(filtered);
+    }
+  }, [selectedGenre]); // dipendenza che scatena l'effetto//
+
+  return (
+    <div className="container">
+      <h1>Filtra Film per genere</h1>
+
+      <GenreFilter
+        genres={genres}
+        selectedGenre={selectedGenre}
+        onGenreChange={setSelectedGenre}
+      />
+
+      <MovieList movies={filteredMovies} />
+    </div>
+  );
 }
 
 export default App;
