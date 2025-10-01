d81a95b801213a43987da7c370f26d758f9ce778
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-10-01 15:52:52 +0200

    creati components e primo sviluppo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { useState } from "react";
+import { useState, userEffect } from "react";
+import MovieList from "./components/MovieList";
+import GenreFilter from "./components/GenreFilter";
 import "./App.css";
+
+const initialMovies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
 
 function App() {
   return <></>;
